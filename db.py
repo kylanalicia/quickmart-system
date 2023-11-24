198aa2dfcf2c840dbed9fd91250b20766dcdffdb
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2023-11-24 19:02:41 +0300

    create session in db.py

---
+++ db.py
@@ -0,0 +1,14 @@
+from sqlalchemy import MetaData,create_engine
+from sqlalchemy.orm import sessionmaker,declarative_base
+
+convention = {
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
+
+Base = declarative_base(metadata=metadata)
+
+# Create an SQLAlchemy engine and session
+engine = create_engine('sqlite:///database.db')
+Session = sessionmaker(bind=engine)
+session = Session()
